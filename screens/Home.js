bfce5e30e4aa3025512c079e41d1cf42f3ca611a
suryaprakash <surya.zvky@gmail.com>
2020-11-29 17:14:56 +0530

    getting seasons data from local storage to display

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,86 @@
-import React from 'react';
-import {Text, StyleSheet, ScrollView} from 'react-native';
-import {Fab, Icon} from 'native-base';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
+import {
+  Fab,
+  Icon,
+  List,
+  ListItem,
+  Left,
+  Right,
+  Body,
+  CheckBox,
+  Container,
+  H1,
+  Button,
+  Title,
+  Text,
+  Spinner,
+} from 'native-base';
+
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Home = ({navigation, route}) => {
+  const [seasons, setSeasons] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getSeasonList = async () => {
+    setLoading(true);
+    const storedValue = await AsyncStorage.getItem('@session_list');
+    if (!storedValue) {
+      setSeasons([]);
+    }
+    const list = JSON.parse(storedValue);
+
+    setSeasons(list);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getSeasonList();
+  }, [seasons]);
+
+  //   if (loading) {
+  //     return (
+  //       <Container style={styles.container}>
+  //         <Spinner color="green" />
+  //       </Container>
+  //     );
+  //   }
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text>List of sessions goes here</Text>
+      {seasons.length == 0 ? (
+        <Container style={styles.container}>
+          <H1 style={styles.heading}>Watchlist is empty.Please add to watch</H1>
+        </Container>
+      ) : (
+        <>
+          <H1 style={styles.heading}>Next series to watch</H1>
+          {seasons.map((season) => (
+            <List key={season.id}>
+              <ListItem style={styles.listItem} noBorder>
+                <Left>
+                  <Button style={styles.actionButton} danger>
+                    <Icon name="trash" />
+                  </Button>
+                  <Button style={styles.actionButton}>
+                    <Icon name="edit" type="Feather" />
+                  </Button>
+                </Left>
+                <Body>
+                  <Title style={styles.seasonName}>{season.name}</Title>
+                  <Text note>{season.noOfSessions}</Text>
+                </Body>
+                <Right>
+                  <CheckBox />
+                </Right>
+              </ListItem>
+            </List>
+          ))}
+        </>
+      )}
+
       <Fab
         style={{backgroundColor: '#5067FF'}}
         position="bottomRight"
